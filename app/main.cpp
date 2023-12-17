7f6dfadb5adebf3b1797c26c897dcfe4c62b69d9
jakubg05 <jakub.gordiak@gmail.com>
2023-12-17 10:59:41 +0100

    initial commit

---
+++ app/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <my_lib.hpp>
+
+int main() {
+    hello();
+    return 0;
+}
---
+++ lib/my_lib/my_lib.cpp
@@ -0,0 +1,6 @@
+#include "my_lib.hpp"
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello" << std::endl;
+}
